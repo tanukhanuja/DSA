9e16425ce3cc997aa450685459dbbc60ffcae72d
tanukhanuja <113044294+tanukhanuja@users.noreply.github.com>
2022-09-12 12:46:28 +0530

    Update 2_bubble_sort_nested.cpp

--- 2_bubble_sort_nested.cpp
+++ 2_bubble_sort_nested.cpp
@@ -1,6 +1,10 @@
 #include<iostream>
 using namespace std;
 
+//In bubble sort we compare adjacent values
+//can be done using nested loop, recursion etc
+
+//define bubb;e sorting function with nested loop
 void bubble_sort(int arr[], int size)
 {
     int s = size;
@@ -14,7 +18,15 @@ void bubble_sort(int arr[], int size)
             arr[j]=arr[j+1];
             arr[j+1]=x;
             }
+            //print array after each pass
+        cout << "array after pass " << i+1 << ":  ";
+        for(int k=0; k<size; k++)
+        {
+            cout << arr[k] << " ";
         }
+        cout << endl;
+        }
+        
         s--;
     }
 }
@@ -23,10 +35,22 @@ int main()
 {
     int arr[]={5,1,4,2,8,1,3,2};
     int size=sizeof(arr)/sizeof(arr[0]);
-    bubble_sort(arr,size);
-    for(int i=0; i<size; i++)
+   
+    //print given array
+    cout << "unsorted array:  ";
+    for (int i=0; i<size; i++)
     {
         cout << arr[i] << " ";
+    }
+    cout << endl;
+    
+    bubble_sort(arr,size);
+
+    //print sorted array
+    cout << "sorted array is:  ";
+    for(int i=0; i<size; i++)
+    {
+        cout << arr[i] << "  ";
     }
     return 0;
 }
